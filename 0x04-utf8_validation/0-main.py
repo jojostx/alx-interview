5361cfb1c968769c81a2f90227afbb54b531b91c
jojostx <ikuskid7@gmail.com>
2023-07-31 09:43:41 +0100

    correction 1

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python3
 """
 Main file for testing
+  # check an integer in the list
+  # consider only the least 8 significant bits
+  # count = the number of bits from the most significant position that are one's,
+  # if count > 4:
+  #   return false
+  # else: // count is less than 4
+  #   look forward in the list until (count - 1)
+  #       if all (count - 1) bytes donot have their most significant 2 bits being 10
+  #           return false
+  #       else
+  #           continue outer loop at count index if count index is less than length of data.
 """
 
 validUTF8 = __import__('0-validate_utf8').validUTF8
 
 data = [65]
-print(validUTF8(data))
+print(validUTF8(data)) # True
 
 data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data))
+print(validUTF8(data)) # True
+
+data = [467, 133, 108]
+print(validUTF8(data)) # True
+
+data = [240, 188, 128, 167]
+print(validUTF8(data)) # True
+
+data = [197, 130, 1]
+print(validUTF8(data)) # True
 
 data = [229, 65, 127, 256]
-print(validUTF8(data))
+print(validUTF8(data)) # False
+
+data = [110, 130, 1]
+print(validUTF8(data)) # False
+
+data = [235, 140, 4]
+print(validUTF8(data)) # False
--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,10 +6,53 @@ def validUTF8(data):
     """Determines if a given data set
     represents a valid utf-8 encoding
     """
-    isValid = True
 
-    for char_code in data:
-        if char_code < 0 or char_code > 127:
-            isValid = False
+    data_lsb = []
 
-    return isValid
+    # map the data list to a new list that contains
+    # the least significant bytes of each integer
+    for v in data:
+        v = bin(v & 0xFF)[2:]
+        while len(v) < 8:
+            v = '0' + v
+        data_lsb.append(v)
+
+    data_counts = []
+    count = 0
+    for v in data_lsb:
+        if v.startswith('110'):
+            count = 2
+        elif v.startswith('1110'):
+            count = 3
+        elif v.startswith('11110'):
+            count = 4
+        elif v.startswith('0'):
+            count = 1
+        else:
+            count = 0
+
+        data_counts.append(count)
+
+    is_valid = True
+    for (i, v) in enumerate(data_counts):
+        if i == 0 and v == 0:
+            is_valid = False
+
+        nxt_ix = i + 1
+        if (v == 1 and nxt_ix < len(data_counts) and data_counts[nxt_ix] == 0):
+            is_valid = False
+
+        if v > 1:
+            c = i + v
+            s = i + 1
+            if s >= len(data_counts):
+                is_valid = False
+            while s < len(data_counts) and (s < c):
+                if data_counts[s] > 0:
+                    is_valid = False
+                s += 1
+
+            i = s
+        pass
+
+    return is_valid
