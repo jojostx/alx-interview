e3e7d6ef89b1a0a74cde6f7b94ff0f2b4ec2a707
jojostx <ikuskid7@gmail.com>
2023-08-28 18:48:55 +0100

    solutions

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,32 +6,28 @@ def island_perimeter(grid):
     """This function will take a list of list of integers and use
        that to calculate how the perimeter of the island described in grid
     """
-    perimeter = j = i = 0
+    perimeter = 0
+
     nc = len(grid)
+    for i in range(nc):
+      nr = len(grid[i])
+      for j in range(nr):
+          if grid[i][j] == 1:
+              above = i - 1
+              left = j - 1
+              right = j + 1
+              below = i + 1
 
-    while i < nc:
-        while j < nc:
-            n = grid[i][j]
-            if n == 1:
-                above = i - 1
-                left = j - 1
-                right = j + 1
-                below = i + 1
+              if above < 0 or grid[above][j] != 1:
+                  perimeter += 1
 
-                if above < 0 or grid[above][j] != 1:
-                    perimeter += 1
+              if left < 0 or grid[i][left] != 1:
+                  perimeter += 1
 
-                if left < 0 or grid[i][left] != 1:
-                    perimeter += 1
+              if right > nr or grid[i][right] != 1:
+                  perimeter += 1
 
-                if right > nc or grid[i][right] != 1:
-                    perimeter += 1
-
-                if below > nc or grid[below][j] != 1:
-                    perimeter += 1
-            j += 1
-
-        i += 1
-        j = 0
+              if below > nc or grid[below][j] != 1:
+                  perimeter += 1
 
     return perimeter
